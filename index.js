f5173d6ffaadb821a099908f005fca5ab5800363
lmy <1429418347@qq.com>
2022-11-02 00:44:54 +0800

    '11.2晚,差顶部底部,还有一些其他bug'

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 console.log('首页');
+var applying = document.getElementsByClassName('applying')[0];
+var people = document.getElementsByClassName('people')[0];
+var numLose = document.getElementsByClassName('numLose')[0];
+var flg = true;
+applying.onclick = function () {
+    var numadd = people.innerHTML;
+    if (flg == true) {
+        numadd++;
+    }
+    people.innerHTML = numadd;
+    var numlose = numLose.innerHTML;
+    numlose--;
+
+    if (numlose <= 0) {
+        this.innerHTML = '申请已满';
+        numlose = 0;
+        this.style.color = '#a3a3a3';
+        var numxaddend = numadd;
+        flg = false;
+        people.innerHTML = numxaddend;
+    }
+    numLose.innerHTML = numlose;
+}
+// 倒计时函数
+// countDown()
+setInterval(countDown, 1000)
+function fn(a) {
+    return a >= 10 ? a : '0' + a;
+}
+function countDown() {
+    var nowTime = new Date();
+    var goTime = new Date(2022, 11, 11);
+    var mistimingTime = goTime - nowTime;
+    var day1 = parseInt(mistimingTime / 1000 / 60 / 60 % 24);
+    var day = fn(parseInt(mistimingTime / 1000 / 60 / 60 % 24));
+    var sh = parseInt(mistimingTime / 1000 / 60 % 60);
+    var sh = fn(parseInt(mistimingTime / 1000 / 60 % 60));
+    var m1 = parseInt(mistimingTime / 1000 % 60);
+    var m = fn(parseInt(mistimingTime / 1000 % 60));
+    var str = day + '天' + sh + '小时' + m + '分钟';
+    var time = document.getElementsByClassName('time')[0];
+    time.innerHTML = str;
+    if (day1 <= 0 && sh1 <= 0 && m1 <= 0 && s1 <= 0) {
+        var timebig = document.getElementsByClassName('timebig')[0];
+        timebig.innerHTML = '申请已结束'
+    }
+
+}
--- try/js/experienceAll.js
+++ try/js/experienceAll.js
@@ -1,1 +1,79 @@
 console.log('试用-体验师-全部');
+// 渲染数据
+
+// 请求数据
+
+var datalis = [];
+var loadimg = document.getElementsByClassName('load')[0].children[0];
+ajaxfn()
+function ajaxfn() {
+    var ajax_ = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTT');
+    ajax_.open('get', 'http://127.0.0.1:3000/useing/master', true);
+    ajax_.send();
+    ajax_.onreadystatechange = function () {
+        if (ajax_.readyState == 4) {
+            if (ajax_.status == 200) {
+                var data = JSON.parse(ajax_.responseText);
+                datalis = data;
+                show(datalis);
+                loadimg.src = './../img/more.png'
+            } else {
+                console.log('请求失败');
+            }
+        }
+    }
+}
+var str = '';
+function show(a) {
+    for (var item of a) {
+        if (item.apply == undefined || item.totalnum == undefined || item.num == undefined) {
+            item.apply = '1392';
+            item.totalnum = '2032';
+            item.num = '20'
+        }
+        item.info_ty = '体验师专享';
+        // console.log(item.info_ty == undefined);
+        if (item.info_ty == '首发') {
+            str += `
+            <div>
+            <em>${item.info_ty}</em>
+            <img src="${item.img}">
+            <div class="describe">${item.text}</div>
+            <div class="s_num">
+                <span>${item.totalnum}</span>
+                <span>${item.num}</span>
+            </div>
+            <div class="s_apply"><span>${item.apply}</span>申请</div>
+            <div class="s_time">剩余2天</div>
+        </div>
+            `
+        } else {
+            str += `
+            <div>
+            <em id="sign">${item.info_ty}</em>
+            <img src="${item.img}">
+            <div class="describe">${item.text}</div>
+            <div class="s_num">
+                <span>${item.totalnum}</span>
+                <span>${item.num}</span>
+            </div>
+            <div class="s_apply"><span>${item.apply}</span>申请</div>
+            <div class="s_time">剩余2天</div>
+        </div>
+            `
+        }
+
+    }
+    var s_content = document.getElementsByClassName('s_content')[0];
+    s_content.innerHTML = str;
+}
+// 点击加载更多
+var load = document.getElementsByClassName('load')[0];
+load.onclick = function () {
+    loadimg.src = './../img/loading-icon.gif'
+    //    setti show(datalis)
+    setTimeout(ajaxfn, 2000);
+
+}
+
+
